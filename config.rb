6f8380460412d1cc16ce09bd4a499d470f9894fd
Chris Lloyd <christopher.lloyd@gmail.com>
2013-03-17 22:21:21 -0700

    Don't require PartyCloud class.

--- config.rb
+++ config.rb
@@ -1,18 +1,13 @@
 require 'redis'
 require 'redis/connection/synchrony'
 
-require 'tron/party_cloud'
-
 require 'active_record'
 require 'sync-em/pg'
 
 # Setup a new Redis connection pool
-config['redis'] = EventMachine::Synchrony::ConnectionPool.new(size: 5) do
+config[:redis] = EventMachine::Synchrony::ConnectionPool.new(size: 5) do
   Redis.new
 end
-
-# Connect to the PartyCloud redis instance
-PartyCloud.redis = config['redis']
 
 # Initialize ActiveRecord
 ActiveRecord::Base.establish_connection(ENV['DATABASE_URL'])
