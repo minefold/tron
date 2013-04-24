5ca5f659b3ddedf3d58e4c9a4b93d65cac958b86
Chris Lloyd <christopher.lloyd@gmail.com>
2013-04-24 13:21:50 -0700

    Notes on hard-coded values to get Tron working with Minefold.

--- db/seeds.rb
+++ db/seeds.rb
@@ -17,13 +17,15 @@ DB[:accounts].insert(
 
 # Regions
 
+# The region id is hardcoded in Minefold.
 DB[:regions].insert(
-  id: SecureRandom.uuid,
+  id: '71519ec0-1515-42b9-b2f6-a24c151a6247',
   name: 'us-east-1',
 )
 
 # Existing funpacks
 
+# These funpack ids are hardcoded in Minefold.
 {
   'minecraft'                 => '9ed10c25-60ed-4375-8170-29f9365216a0',
   'bukkit-essentials'         => 'c942cbc1-05b2-4928-8695-b0d2a4d7b452',
