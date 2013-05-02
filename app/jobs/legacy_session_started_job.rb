d43204e32262ad7fae114db673fdd5cbd04f8e29
Dave Newman <dave@snappyco.de>
2013-05-01 18:29:45 -0700

    update legacy session started job for Minefold, which doesn't make the start http call

--- app/jobs/legacy_session_started_job.rb
+++ app/jobs/legacy_session_started_job.rb
@@ -3,9 +3,19 @@ class LegacySessionStartedJob
 
   def perform(server_id, ts, ip, port)
     if server = Server.where(legacy_id: server_id).first
-      if session = server.session
-        SessionStartedJob.perform_async session.id, DateTime.rfc3339(ts).rfc3339, ip, port
+      if server.up? || server.starting?
+        return
       end
+
+      session = Session.new(id: SecureRandom.uuid, server: server)
+
+      # TODO Rescue transaction failure
+      DB.transaction do
+        session.save
+        server.start!
+      end
+
+      SessionStartedJob.perform_async session.id, DateTime.rfc3339(ts).rfc3339, ip, port
     end
   end
 end
