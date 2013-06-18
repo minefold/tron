182152146e3f006c4e6f06c880bda34b4c9ff821
Dave Newman <dave@snappyco.de>
2013-06-18 14:57:18 -0700

    Add web endpoints for snapshot deletion

--- app/controllers/snapshots_controller.rb
+++ app/controllers/snapshots_controller.rb
@@ -3,17 +3,41 @@ require 'securerandom'
 require 'brain'
 
 class SnapshotsController < Controller
-  delete '/servers/:id/snapshots' do
-    authenticate!
-    param :id, String, required: true, is: ID_PATTERN, :coerce => :downcase
+  def find_server(id)
+    if params[:id] =~ /legacy-([0-9a-f]+)/
+      Server.where(legacy_id: $1, account: account).first
+    else
+      Server.where(id: params[:id], account: account).first
+    end
+  end
 
-    server = Server.where(id: params[:id], account: account).first
+  post '/servers/:id/snapshots/compact' do
+    authenticate!
+    param :id, String, required: true, :coerce => :downcase
+
+    server = find_server(params[:id])
 
     if server.nil?
       halt 404
     end
 
-    DeleteServerSnapshots.perform_async(server.legacy_id)
+    CompactServerSnapshotsJob.perform_async(server.legacy_id)
+
+    status 200
+  end
+
+
+  delete '/servers/:id/snapshots' do
+    authenticate!
+    param :id, String, required: true, :coerce => :downcase
+
+    server = find_server(params[:id])
+
+    if server.nil?
+      halt 404
+    end
+
+    DeleteServerSnapshotsJob.perform_async(server.legacy_id)
 
     status 200
   end
